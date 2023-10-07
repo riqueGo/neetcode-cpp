1b43d83f062dec5ca339336f390b0cec78631bf2
Henrique Oliveira <henrique.oliveira.gomes@gmail.com>
2023-10-07 16:56:07 -0300

    GenerateParentheses

---
+++ GenerateParentheses/GenerateParentheses.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<string>
+
+using std::vector;
+using std::string;
+
+class Solution {
+private:
+    int _nPairs;
+    vector<string> _result;
+
+    void generate(int open, int close, string str) {
+        if (open == _nPairs && close == _nPairs) {
+            _result.push_back(str);
+            return;
+        }
+        if (open < _nPairs) {
+            generate(open + 1, close, str + '(');
+        }
+        if (open > close) {
+            generate(open, close + 1, str + ')');
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        _nPairs = n;
+        generate(0, 0, "");
+        return _result;
+    }
+};
