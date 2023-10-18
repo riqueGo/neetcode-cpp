6c4e2fb7046ac518521c3998709036bbacba7d8e
Henrique Oliveira <henrique.gomes_de_oliveira@syngenta.com>
2023-10-18 15:07:23 -0300

    Weighted Graph

--- DataStructure/Graph/Graph.cpp
+++ DataStructure/Graph/Graph.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 using std::vector;
+using std::pair;
 
 //Space O(n²)
 void matrixUndirectedGraph() {
@@ -30,6 +31,33 @@ void matrixDirectedGraph() {
     }
 }
 
+//Space O(n²)
+void matrixUndirectedWeightedGraph() {
+    int n, m;
+    std::cin >> n >> m;
+
+    int adj[n+1][n+1];
+    for (int i = 0; i < m; i++){
+        int u, v, wt;
+        std::cin >> u >> v >> wt;
+        adj[u][v] = wt;
+        adj[v][u] = wt;
+    }
+}
+
+//Space O(n²)
+void matrixDirectedWeightedGraph() {
+    int n, m;
+    std::cin >> n >> m;
+
+    int adj[n+1][n+1];
+    for (int i = 0; i < m; i++){
+        int u, v, wt;
+        std::cin >> u >> v >> wt;
+        adj[u][v] = wt;
+    }
+}
+
 //Space O(2*E)
 void listUndirectedGraph() {
     int n, m;
@@ -54,5 +82,32 @@ void listDirectedGraph() {
         int u, v;
         std::cin >> u >> v;
         adj[u].push_back(v);
+    }
+}
+
+//Space O(2*E)
+void listUndirectedWeightedGraph() {
+    int n, m;
+    std::cin >> n >> m;
+
+    vector<pair<int,int>> adj[n+1];
+    for (int i = 0; i < m; i++) {
+        int u, v, wt;
+        std::cin >> u >> v >> wt;
+        adj[u].push_back(pair(u, wt));
+        adj[v].push_back(pair(u, wt));
+    }
+}
+
+//Space O(2*E)
+void listDirectedWeightedGraph() {
+    int n, m;
+    std::cin >> n >> m;
+
+    vector<pair<int,int>> adj[n+1];
+    for (int i = 0; i < m; i++) {
+        int u, v, wt;
+        std::cin >> u >> v >> wt;
+        adj[u].push_back(pair(u, wt));
     }
 }
